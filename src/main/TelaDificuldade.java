5f617da9aa9a62e18f88fea6b9c31639b411cd61
Gbets912 <gustavobdominguez@gmail.com>
2025-05-20 21:27:11 -0300

    Update TelaDificuldade.java

--- src/main/TelaDificuldade.java
+++ src/main/TelaDificuldade.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 
 public class TelaDificuldade extends JFrame {
     public TelaDificuldade() {
-        setTitle("Parabéns!");
+        setTitle("Selecionar Dificuldade!");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 600); // Tamanho padrão caso não maximize
         setLocationRelativeTo(null);
